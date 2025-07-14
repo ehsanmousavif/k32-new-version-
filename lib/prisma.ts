a3eb0be70c347e971cd6611daf7ba0e1c2f8ced5
ehsanmousavif <ehsan.mousavi.f12345@gmail.com>
2025-07-14 17:31:52 +0430

    c

---
+++ lib/prisma.ts
@@ -0,0 +1,30 @@
+// lib/prisma.ts
+import { PrismaClient } from "@/generated/prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | null;
+};
+
+let prisma: PrismaClient;
+
+if (!globalForPrisma.prisma) {
+  try {
+    prisma = new PrismaClient();
+    globalForPrisma.prisma = prisma;
+    console.log("✅ Prisma connected.");
+  } catch (error) {
+    console.error("❌ Error connecting to Prisma:", error);
+    globalForPrisma.prisma = null;
+  }
+} else {
+  prisma = globalForPrisma.prisma;
+}
+
+export async function disconnectPrisma() {
+  if (prisma) {
+    await prisma.$disconnect();
+    console.log("🔌 Prisma disconnected.");
+  }
+}
+
+export { prisma };
