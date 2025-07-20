cde8a06a778705b69387c41dac19c43c5f3f47e5
ehsanmousavif <ehsan.mousavi.f12345@gmail.com>
2025-07-20 22:11:45 +0430

    c

---
+++ app/api/internal/validetet-card/route.ts
@@ -0,0 +1,29 @@
+import { db } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { cardNumber, iban, ownerName } = await req.json();
+
+  if (!cardNumber) {
+    return NextResponse.json(
+      { error: "شماره کارت الزامی است" },
+      { status: 400 }
+    );
+  }
+
+  const checkCardNumber = await db.validatedCard.findUnique({
+    where: { cardNumber },
+  });
+
+  if (checkCardNumber) {
+    // اگر قبلاً ذخیره شده بود، همونو برمی‌گردونه
+    return NextResponse.json(checkCardNumber);
+  }
+
+  // اگر نبود، ثبت می‌کنیم و همون رو برمی‌گردونیم
+  const newCard = await db.validatedCard.create({
+    data: { cardNumber, iban, ownerName },
+  });
+
+  return NextResponse.json(newCard); // این خط اضافه بشه تا response برگرده
+}
